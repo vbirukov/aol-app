fccfea93b144e76fc32749d4bfd9dc2bd0c31efd
Vitaliy Biryukov <vbirukov@gmail.com>
2020-08-19 12:10:20 +0300

    meterial UI added

--- src/ui/PaperButton/PaperButton.tsx
+++ src/ui/PaperButton/PaperButton.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import Icon from "@material-ui/core/Icon";
 import './PaperButton.css';
 
 interface IProps {
     caption: String;
     iconName: String;
+    clickHandler: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
 export default function PaperButton(props: IProps) {
     return (
         <Paper
+            onClick={props.clickHandler}
             className="PaperButton">
-            <h3>{props.caption}</h3>
+            <Typography variant="button" display="block" gutterBottom>
+                {props.caption}
+            </Typography>
             <Icon>{props.iconName}</Icon>
         </Paper>
     );
--- src/ui/Workspace/Workspace.tsx
+++ src/ui/Workspace/Workspace.tsx
@@ -3,6 +3,9 @@ import './Workspace.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import PaperButton from "../PaperButton/PaperButton";
+import Container from "@material-ui/core/Container";
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
 
 type ButtonMeta = {
     caption: String;
@@ -14,24 +17,46 @@ interface IProps {
 }
 
 export default function SimpleContainer(props: IProps) {
+
+    const [VideoOpen, setOpen] = React.useState(false);
+
+    const handleClickOpen = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        setOpen(true);
+    };
+
+    const handleClose = (value:boolean) => {
+        setOpen(false);
+    };
+
     return (
         <React.Fragment>
-            <CssBaseline />
-            <Grid
-                container
-                direction="row"
-                justify="center"
-                alignItems="center"
-                spacing={2}
-            >
-                { props.items.map((item, index) => {
-                    return <Grid item xs={4}>
+            <Container maxWidth="sm">
+                <CssBaseline />
+                <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    spacing={2}
+                >
+                    { props.items.map((item, index) => {
+                        return <Grid item xs={4}>
                             <PaperButton
-                                key={index}
-                                caption={item.caption}
-                                iconName={item.iconName} /> </Grid>
-                })}
-            </Grid>
+                                    key={index}
+                                    clickHandler={handleClickOpen}
+                                    caption={item.caption}
+                                    iconName={item.iconName} /> </Grid>
+                    })}
+                </Grid>
+
+                <Dialog aria-labelledby="simple-dialog-title" open={VideoOpen}  onClose={handleClose}>
+                    <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/IbT9D1UWoik" frameBorder="0"
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen></iframe>
+                </Dialog>
+
+            </Container>
         </React.Fragment>
     );
 }
